e97b1d29a6fad52ae55d461de7eb4a0a1c7309b5
PaulSebalu <paolossebalu@gmail.com>
2022-02-26 10:01:26 +0300

    chore: add schema

---
+++ account/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self, email, password=None, is_staff=False, is_active=True, **extra_fields
+    ):
+        email = UserManager.normalize_email(email)
+        user = self.model(
+            email=email, is_active=is_active, is_staff=is_staff, **extra_fields
+        )
+        if password:
+            user.set_password(password)
+        user.save()
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self.create_user(
+            email, password, is_staff=True, is_superuser=True, **extra_fields
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=256, blank=True)
+    last_name = models.CharField(max_length=256, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now, editable=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __repr__(self):
+        pass
---
+++ travel/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.conf import settings
+
+
+class FlightStatus:
+    SCHEDULED = "scheduled"
+    CANCELLED = "cancelled"
+    DEPARTED = "departed"
+
+    CHOICES = [
+        (SCHEDULED, "scheduled"),
+        (CANCELLED, "cancelled"),
+        (DEPARTED, "departed"),
+    ]
+
+
+class BookingStatus:
+    SCHEDULED = "scheduled"
+    CANCELLED = "cancelled"
+
+    CHOICES = [
+        (SCHEDULED, "scheduled"),
+        (CANCELLED, "cancelled"),
+    ]
+
+
+class Flight(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True)
+    airline_name = models.CharField(max_length=256, blank=True)
+    air_craft_type = models.CharField(max_length=256, blank=True)
+    departure = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(
+        max_length=30, choices=FlightStatus.CHOICES, default=FlightStatus.SCHEDULED
+    )
+    route = models.ManyToManyField(
+        Routes,
+        db_table=flight_routes,
+        related_name="routes",
+        related_query_name="route",
+    )
+    flight_number = models.CharField(max_length=30, blank=True)
+    number_of_seats = models.PositiveIntegerField(blank=True, null=True)
+
+    def __repr__(self):
+        pass
+
+
+class Booking(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    flight = models.ForeignKey(
+        Flight,
+        blank=True,
+        null=True,
+        related_name="bookings",
+        on_delete=models.SET_NULL,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        related_name="bookings",
+        on_delete=models.SET_NULL,
+    )
+    status = models.CharField(
+        max_length=30, choices=BookingStatus.CHOICES, default=BookingStatus.SCHEDULED
+    )
+
+    def __repr__(self):
+        pass
+
+
+class Routes(models.Model):
+    name = models.CharField(max_length=256, blank=True)
